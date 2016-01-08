f1699bd48e378bd660abcf5c58f11369ef84d04a
caihongwei <caihongwei@zenmen.com>
2016-01-08 19:03:40 +0800

    1、重构； 2、创建thymeleaf模块；

--- thymeleaf/src/test/java/com/hong/ThymeleafApplicationTests.java
+++ thymeleaf/src/test/java/com/hong/ThymeleafApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootDemoApplication.class)
+@SpringApplicationConfiguration(classes = ThymeleafApplication.class)
 @WebAppConfiguration
-public class SpringBootDemoApplicationTests {
+public class ThymeleafApplicationTests {
 
 	@Test
 	public void contextLoads() {
