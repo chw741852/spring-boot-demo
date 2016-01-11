31d2b77012668dbcc8a2ed5097cfd4d4a5a268e4
caihongwei <caihongwei@zenmen.com>
2016-01-11 19:51:33 +0800

    add velocity demo

---
+++ velocity/src/main/java/com/hong/WelcomeController.java
@@ -0,0 +1,24 @@
+package com.hong;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+/**
+ * Created by caihongwei on 2016/1/11 19:04.
+ */
+@Controller
+public class WelcomeController {
+    @Value("${application.message:Hello World}")
+    private String message;
+
+    @RequestMapping("/")
+    public String welcome(Model model) throws InterruptedException {
+        model.addAttribute("time", new Date());
+        model.addAttribute("message", this.message);
+        return "welcome";
+    }
+}
