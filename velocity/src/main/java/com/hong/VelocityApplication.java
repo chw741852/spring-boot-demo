97082b8f38b97623c7e2fce0411da97c9af119e9
caihongwei <caihongwei@zenmen.com>
2016-01-14 19:39:20 +0800

    update some maven dependencies

--- velocity/src/main/java/com/hong/VelocityApplication.java
+++ velocity/src/main/java/com/hong/VelocityApplication.java
@@ -1,5 +1,6 @@
 package com.hong;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VelocityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VelocityApplication.class, args);
+//		SpringApplication.run(VelocityApplication.class, args);
+        SpringApplication app = new SpringApplication(VelocityApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
 	}
 }
--- velocity/src/main/java/com/hong/WelcomeController.java
+++ velocity/src/main/java/com/hong/WelcomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
@@ -20,5 +21,11 @@ public class WelcomeController {
         model.addAttribute("time", new Date());
         model.addAttribute("message", this.message);
         return "welcome";
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "The Test Page!";
     }
 }
