f7c568092f36378c57d8ad1f707c448b0f8ef44f
Hongwei <caihongwei2008@gmail.com>
2016-01-24 12:25:05 +0800

    update log4j2 configuration

--- velocity/src/main/java/com/hong/VelocityApplication.java
+++ velocity/src/main/java/com/hong/VelocityApplication.java
@@ -1,11 +1,17 @@
 package com.hong;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VelocityApplication {
 	public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(VelocityApplication.class);
 		SpringApplication.run(VelocityApplication.class, args);
+		logger.info("Test Log4j2 info");
+		logger.warn("Test Log4j2 warn");
+		logger.error("Test log4j2 error");
 	}
 }
