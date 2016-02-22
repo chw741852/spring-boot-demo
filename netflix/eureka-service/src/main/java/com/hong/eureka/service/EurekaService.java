09448a87ddeacbd3cb8319cfb642db4ce1c6c3fa
caihongwei <caihongwei@mochui.com>
2016-02-22 19:40:37 +0800

    add eureka service

---
+++ netflix/eureka-service/src/main/java/com/hong/eureka/service/EurekaService.java
@@ -0,0 +1,31 @@
+package com.hong.eureka.service;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.MyDataCenterInstanceConfig;
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.discovery.DefaultEurekaClientConfig;
+import com.netflix.discovery.DiscoveryManager;
+import com.netflix.discovery.EurekaClient;
+
+/**
+ * Created by caihongwei on 16/2/22.
+ */
+public class EurekaService {
+    public static void main(String[] args) {
+        DynamicPropertyFactory configInstance = DynamicPropertyFactory.getInstance();
+        ApplicationInfoManager applicationInfoManager = ApplicationInfoManager.getInstance();
+
+        DiscoveryManager.getInstance().initComponent(
+                new MyDataCenterInstanceConfig(),
+                new DefaultEurekaClientConfig()
+        );
+
+        EurekaClient eurekaClient = DiscoveryManager.getInstance().getEurekaClient();
+        BaseService baseService = new BaseService(applicationInfoManager, eurekaClient, configInstance);
+        try {
+            baseService.start();
+        } finally {
+            baseService.stop();
+        }
+    }
+}
