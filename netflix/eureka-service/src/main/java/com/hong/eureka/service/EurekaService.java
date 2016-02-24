d1a0e9a5e8060c49bf2cdbb0a13fe4038423dc38
caihongwei <caihongwei@mochui.com>
2016-02-24 15:44:36 +0800

    update eureka property file name and specify it in code

--- netflix/eureka-service/src/main/java/com/hong/eureka/service/EurekaService.java
+++ netflix/eureka-service/src/main/java/com/hong/eureka/service/EurekaService.java
@@ -2,16 +2,25 @@ package com.hong.eureka.service;
 
 import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.MyDataCenterInstanceConfig;
+import com.netflix.config.ConfigurationManager;
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.discovery.DefaultEurekaClientConfig;
 import com.netflix.discovery.DiscoveryManager;
 import com.netflix.discovery.EurekaClient;
+
+import java.io.IOException;
 
 /**
  * Created by caihongwei on 16/2/22.
  */
 public class EurekaService {
     public static void main(String[] args) {
+        try {
+            // specify eureka property file
+            ConfigurationManager.loadCascadedPropertiesFromResources("sample-eureka-service");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         DiscoveryManager.getInstance().initComponent(
                 new MyDataCenterInstanceConfig(),
                 new DefaultEurekaClientConfig()
