503af4972d73d21f10fcce42e1e3d454764b0c5b
caihongwei <caihongwei@mochui.com>
2016-03-06 14:22:35 +0800

    spring cloud eureka connect to multi eureka service

--- rpc/rpc-server/src/main/java/com/hong/RpcServerApplication.java
+++ rpc/rpc-server/src/main/java/com/hong/RpcServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * Created by caihongwei on 2016/2/5 16:33.
  */
 @SpringBootApplication
-@EnableEurekaClient
 public class RpcServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RpcServerApplication.class, args);
