9550eff526a9835a10d6853cf6048efe437a0e52
caihongwei <caihongwei@mochui.com>
2016-03-07 14:25:17 +0800

    add spring cloud hystrix-dashboard module

---
+++ rpc/hystrix-dashboard/src/main/java/com/hong/HystrixDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
