a3f0521ca99fad0670cebeffd3ef9c15b526d1e0
caihongwei <caihongwei@mochui.com>
2016-04-26 17:55:09 +0800

    test

--- rpc/rpc-server/src/main/java/com/hong/RpcServerApplication.java
+++ rpc/rpc-server/src/main/java/com/hong/RpcServerApplication.java
@@ -1,8 +1,10 @@
 package com.hong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class RpcServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RpcServerApplication.class, args);
     }
 
+    @Value("${info.description:local info}")
+    private String info;
+
     @RequestMapping("/hello/{name}")
     public String Hello(@PathVariable String name) {
         return "Hello " + name;
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return info;
     }
 }
